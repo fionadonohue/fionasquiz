ff1aa0e7ae16603d90c797100d15766276a23c42
fionadonohue <97976082+fionadonohue@users.noreply.github.com>
2022-02-02 22:41:52 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
-  
+
+  //question1
+$(".wrong1").click(function() {
+  $("#answer1").text("Try again!")
   });
+
+$("#correct1").click(function() {
+  $("#answer1").text("Good job!")
+});
+
+//q2
+$(".wrong2").click(function() {
+  $("#answer2").text("Try again!")
+  });
+
+$("#correct2").click(function() {
+  $("#answer2").text("Good job!")
+});
+
+//q3
+$(".wrong3").click(function() {
+  $("#answer3").text("Try again!")
+  });
+
+$("#correct3").click(function() {
+  $("#answer3").text("Good job!")
+});
+
+//q4
+$(".wrong4").click(function() {
+  $("#answer4").text("Try again!")
+  });
+
+$("#correct4").click(function() {
+  $("#answer4").text("Good job!")
+});
+
+//5
+$(".wrong5").click(function() {
+  $("#answer5").text("Try again!")
+  });
+
+$("#correct5").click(function() {
+  $("#answer5").text("Good job!")
+});
+
+//6
+$(".wrong6").click(function() {
+  $("#answer6").text("Try again!")
+  });
+
+$("#correct6").click(function() {
+  $("#answer6").text("Good job!")
+});
+});
